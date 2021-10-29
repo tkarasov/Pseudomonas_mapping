11c2c166ecf9c95d86e54bd200141b9fd698c594
Talia <tkarasov@gmail.com>
2021-10-29 16:52:49 -0600

    pixel_plot

--- scripts/image_process_code/pixel_plotting.R
+++ scripts/image_process_code/pixel_plotting.R
@@ -7,15 +7,17 @@ library(heritability)
 library(lme4)
 '%!in%' <- function(x,y)!('%in%'(x,y))
 #now combine pixel information with phylogeny
-phy = read.tree("~/Dropbox/Pseudomonas_1524/phylogeny_data/ps_1524_uncollapsed_5_2018.nwk")
+phy = ape::read.tree("~/Dropbox/Pseudomonas_1524/phylogeny_data/ps_1524_uncollapsed_5_2018.nwk")
 
 #drop irrelevant branches
-pixel = read.table("~/work_remote/Pseudomonas_mapping/data/infection_experiments/june_2018_day7/image_analysis/cell_pixel_strain.txt", header = T, sep = " ")
+pixel = read.table("~/Dropbox/Pseudomonas_trait_mapping/experiments/june_2018_day7/image_analysis/cell_pixel_strain.txt", 
+                   header = T, sep = " ")
 
 pixel_plot = pixel %>% group_by(actual_strain) %>% summarise(mean = mean(pixel_count, na.rm=T), se = sd(pixel_count, na.rm=T) / length(pixel_count))
 tips=phy$tip.label
 not_in = tips[tips %!in% pixel_plot$actual_strain]
 red_phy = drop.tip(phy, not_in)
+red_phy$edge.length <- red_phy$edge.length*0.24
 
 #rownames(pixel_plot) = pixel_plot$actual_strain
 pixel_order = pixel_plot[red_phy$tip.label,]
@@ -25,16 +27,27 @@ pixel_keep = pixel[pixel$actual_strain %in% red_phy$tip.label,]
 pixel_plot_keep = pixel_keep %>% group_by(actual_strain) %>% summarise(mean = mean(pixel_count, na.rm=T), se = sd(pixel_count, na.rm=T) / length(pixel_count))
 
 #now plot the death stuff
-p <- ggtree(red_phy)
+p <- ggtree(red_phy) #+ geom_tiplab(offset=1, hjust=.8, size=2.5)
 p2 <- facet_plot(p, panel = 'dot', data = pixel_plot_keep, geom = geom_segment,aes(x=0, xend=mean, y=y, yend=y), size=.5, color='cyan4')
+p3 <- p + geom_facet(panel = "scatter", data = pixel_plot_keep, geom = geom_point, aes(x=mean))
+p4 <- p + 
+  geom_facet(panel = "scatter", data = pixel_plot_keep, geom = geom_point, aes(x=mean), size=1, color='cyan4') + 
+  geom_facet(panel = "scatter", data = pixel_plot_keep, geom = geom_errorbarh, width = 1,  aes(x=mean, xmin=mean-se, xmax=mean+se)) + 
+
+  theme(strip.background = element_blank(), strip.text = element_blank()) +theme_tree2()
+
+pdf("~/Dropbox/Pseudomonas_trait_mapping/experiments/june_2018_day7/image_analysis/pixel_graph.pdf", family = "ArialMT", height = 2.5, width = 2.5)
+p4
+dev.off()
 
 
 #Estimate heritability
-model1 <- lmer(pixel_count ~ (1|actual_strain), data = pixel, REML = T)
+model1 <- lmer(pixel_count ~ (1|actual_strain) + Plate, data = pixel, REML = T)
+
 vc =VarCorr(model1)
 residual_var = attr(vc, 'sc')^2
 intercept_var = attr(vc$actual_strain,'stddev')[1]^2
-H = intercept_var/(residual_var + intercept_var)
+H2 = intercept_var/(residual_var + intercept_var)
 
 #or not by hand
 herit = repeatability(pixel$pixel_count, pixel$actual_strain)
